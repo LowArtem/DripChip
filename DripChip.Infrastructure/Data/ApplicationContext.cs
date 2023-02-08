c3c122caa29a148ace4762c45bfe6388d2086240
Artem Zhikharev <artemziharew@gmail.com>
2023-02-08 18:57:10 +0300

    add database init settings and test connection envs

---
+++ DripChip.Infrastructure/Data/ApplicationContext.cs
@@ -0,0 +1,34 @@
+﻿using DripChip.Core.Entities;
+using DripChip.Core.Exceptions;
+using Microsoft.EntityFrameworkCore;
+
+namespace DripChip.Infrastructure.Data;
+
+public class ApplicationContext : DbContext
+{
+    public virtual DbSet<User> Users { get; set; }
+    public virtual DbSet<AnimalLocationPoint> LocationPoints { get; set; }
+    public virtual DbSet<AnimalType> AnimalTypes { get; set; }
+    public virtual DbSet<VisitedLocation> VisitedLocations { get; set; }
+    public virtual DbSet<Animal> Animals { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (optionsBuilder.IsConfigured) return;
+        
+        var pgHost = Environment.GetEnvironmentVariable("POSTGRES_HOST");
+        var pgPort = Environment.GetEnvironmentVariable("POSTGRES_PORT");
+        var pgUser = Environment.GetEnvironmentVariable("POSTGRES_USER");
+        var pgPass = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
+        var pgDb = Environment.GetEnvironmentVariable("POSTGRES_NAME");
+
+        if (pgHost == null || pgPort == null || pgUser == null || pgPass == null || pgDb == null)
+        {
+            throw new EnvironmentVariableNotFoundException(nameof(pgHost), nameof(pgPort), 
+                nameof(pgUser), nameof(pgPass), nameof(pgDb));
+        }
+
+        var connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+        optionsBuilder.UseNpgsql(connStr);
+    }
+}
---
+++ DripChip.Infrastructure/Data/Configurations/AnimalConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using DripChip.Core.Entities;
+using DripChip.Core.Enums;
+using DripChip.Core.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DripChip.Infrastructure.Data.Configurations;
+
+public class AnimalConfiguration : IEntityTypeConfiguration<Animal>
+{
+    public void Configure(EntityTypeBuilder<Animal> builder)
+    {
+        builder.Property(t => t.Gender)
+            .IsRequired()
+            .HasConversion(
+                convertToProviderExpression: v => v.ToString(),
+                convertFromProviderExpression: v => EnumExtensions.ParseEnumThrowable<Gender>(v)
+            );
+
+        builder.Property(t => t.LifeStatus)
+            .IsRequired()
+            .HasConversion(
+                convertToProviderExpression: v => v.ToString(),
+                convertFromProviderExpression: v => EnumExtensions.ParseEnumThrowable<LifeStatus>(v)
+            );
+    }
+}
