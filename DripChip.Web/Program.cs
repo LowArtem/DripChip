ca6b64b5d1f93be06bec369015f75c15961e0d73
Artem Zhikharev <artemziharew@gmail.com>
2023-02-10 22:59:08 +0300

    add auth support in swagger
    To be able to test authorized only requests with swagger

--- DripChip.Web/Program.cs
+++ DripChip.Web/Program.cs
@@ -3,6 +3,7 @@ using DripChip.Infrastructure.Repositories;
 using DripChip.Infrastructure.Services;
 using DripChip.Web.Auth;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,7 +19,35 @@ builder.Services.AddAuthentication("BasicAuthentication")
     .AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>("BasicAuthentication", null);
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Basic", new OpenApiSecurityScheme
+    {
+        Description = "Enter your credentials (in field named \"Username\" enter your email)",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "Basic"
+    });
+    
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Basic"
+                },
+                Scheme = "Basic",
+                Name = "Basic",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
 
 var app = builder.Build();
 
